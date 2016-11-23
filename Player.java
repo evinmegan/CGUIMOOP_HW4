3ff155a1b89f013183a74325085306ce1903967e
evinmegan <evinmegan@gmail.com>
2016-11-23 13:24:03 +0800

    HW4

---
+++ Player.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Player {
+       private String name;
+       private int chips;
+       private int bet = 0;
+       private ArrayList<Card> oneRoundCard;
+       
+       public Player(String name, int chips) {
+    	   this.name = name;
+    	   this.chips = chips;
+       }
+       
+       public String get_name() {
+    	   return name;
+       }
+       
+       public int make_bet() {
+    	   if (chips > 0) {
+    		   bet = 1;
+    		   //chips -= 1;
+    	   }
+    	   else {
+    		   bet = 0;
+    		   System.out.print("come on.. .There is no money anymore");
+    	   }  
+    	   return bet;
+       }
+       
+       public void setOneRoundCard(ArrayList<Card> cards) {
+    	   oneRoundCard = cards;
+       }
+       
+       public boolean hit_me(){
+    	       boolean yesOrNo = false;
+    		   if (getTotalValue() <= 16){
+    			  yesOrNo = true;
+    		   }
+    	   return yesOrNo;
+       }
+       
+       public int getTotalValue() {
+    	   int value = 0;
+    	   for (Card element : oneRoundCard){
+    		   value += element.getRank();
+    		   if (element.getRank() == 1) {
+    			   if (new Random().nextInt(2) == 0) {
+    				   value += 10;
+    			   }	   
+    		   }
+    	   }
+    	   return value;
+       }
+       
+       public int get_current_chips() {
+    	   return chips;
+       }
+       
+       public void increase_chips(int diff){
+    	   chips += diff;
+       }
+       public void say_hello() {
+    	   System.out.println("Hello, I am " + name + ".");
+    	   System.out.println("I have " + chips + " chips.");
+       }
+       
+}
